5058270b211d06da8b0be09ac862aa680dd7d24b
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-02-15 20:40:06 -0600

    segunda actualización

--- totalDeManzanas.py
+++ totalDeManzanas.py
@@ -1,1 +1,7 @@
-cantidad de manzanas+ flotat(input("ingresa )
+cantidadDeManzanas = float(input("1.	Ingresa la cantidad ed manzanas que venderas:   "))
+precioDeManzanas = float(input("2.	Ingresa el precio de la manzanas:   "))
+
+totalApagar = cantidadDeManzanas * precioDeManzanas
+print("Total a pagar ", totalApagar)
+print("")
+
