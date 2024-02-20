51949c7353d67529b6f4f4679192b50215149c84
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-02-20 15:22:03 -0600

    Nueva actualización

--- Manzanas.py
+++ Manzanas.py
@@ -13,8 +13,8 @@ pago = (precio * cantidad)
 if cantidad >= 10 :
     descuento = pago * 0.1
     pago = pago - descuento
-    print(f"Se pago {pago} a la cual ya se le aplico en descuento en monto de {descuento}")
-
-if cantidad < 10 :
+    print(f"Se pago {pago} a la cual ya se le aplico en descuento en monto de {descuento}.")
+else :
     print(f"Se pago  {pago}")
+    print("Gracias por su compra.")
 
