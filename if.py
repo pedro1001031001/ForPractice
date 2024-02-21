a021828ef5e4cb2952231087792ada7b88076ed4
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-02-20 18:22:59 -0600

    Corrección

--- if.py
+++ if.py
@@ -1,9 +1,14 @@
 edad = 0
 esEstudiante = "Si"
 dinero = 500
+eresNarco = "Si"
 
 if edad >= 18 and esEstudiante =="Si" or dinero >= 500 :
     print("Puede entrar")
     print("Gasta mucho")
+elif eresNarco == "Si"
+    print("Puede pasar")
+else :
+    print("Adios")
 
 print("Hola")
