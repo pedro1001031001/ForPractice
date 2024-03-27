d5bef0929a23940b506eff1a45f6c8d3bd9fef20
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-03-27 10:01:50 -0600

    Aprendizaje con php

---
+++ php/hello, world.php
@@ -0,0 +1,15 @@
+<?php
+    
+    //  Hola munda
+    echo "hola, pHP";
+    /*
+        Esto es un comentario de varias líneas
+    */
+
+    //  Variables
+    $my_string = "Esto es una cadena de texto";
+
+    echo $my_string;
+
+
+
