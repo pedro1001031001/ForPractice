0309c5d313981a4fe2a2bbc50e06c23b69eb6bcb
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-04-01 10:09:49 -0600

    Curso kotlin

---
+++ kotlin/Curso de Youtube/hello.kt
@@ -0,0 +1,25 @@
+/*
+    Usa lo mismo que java
+    Java virtual machine
+    No interactua con java, usa su principio
+    Es de código abierto
+    Evoluciona java, es iteroperable con java, puede interatuarse con las librerias de java
+    Potencia los productos de la empresa
+    No se preocupa por la migración
+    Es el lenguaje principal para desarrollar aplicaciones en android
+    No han caido en los ,is,os errores que java
+    Es multiplataforma y multiparadigmatico
+    Se adapta a los diferentes entornos
+    Usos:  aplicaciones nativas en android, para crear back-end.
+    Todo lo que funciona con java funciona con kotlin.
+    Da soporte para crear webs,para crear aplicaiones plataformas, alicaiones de escritorio, aplicaiones de data science, estan en una etapa inicial, no estan del todo preparado.
+    Se puede usar una misma base de código para ios android, se hace en partes diferentes la parte visual
+    Valida data
+    En ios, genera un códio c que le permita enender, optimiza todo, se tiene todo compilado.
+    Se piuede decidir en que momenot se puede usar la multiplataforma.
+*/
+
+fun main(){
+    println("Hola, kotlin")
+}
+
