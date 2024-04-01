2d6928983d95bbde548178769e4515952525bd43
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-04-01 14:28:38 -0600

    Curso JavaScript

---
+++ javascript/Curso de Youtube/hello.js
@@ -0,0 +1,22 @@
+/*
+    
+    Fue concebido para la web.
+    Se programa con html y css
+    Si senecesitaba algo más }
+    Funciona en el explorador de web
+    Es el lenguaje de la web
+    Entiende todos los exploradores web
+    Java es diferente de JavaScript
+    Java era la palabra de moda
+    Es interpretado.
+    Se basa en la sintaxis de Java
+    ECMAScript. Es la especificación.
+    Se emplea para el desarrollo front-end.
+    JavaScript se puede ejecutar en el servidor.
+    Se emplea frameworks
+    Se puede emplear para hacer para aplicaiones de escritorio y moviles con herramientas especificas
+    MongoDB esta montado sobre el lenguaje
+    
+*/
+
+console.log("Hola, java")
