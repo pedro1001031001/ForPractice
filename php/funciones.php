85c7f3727c457462c253c953c71fb894e029a461
Damián Rodríguez <piteraguilar2016@gmail.com>
2024-04-03 22:53:56 -0600

    Curso PHP

--- php/funciones.php
+++ php/funciones.php
@@ -1,5 +1,15 @@
 <?php 
 
+    function loremBy(){
+        return "Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Neque libero vero corrupti omnis? Beatae magnam maiores veritatis doloremque harum! Quas repudiandae, est incidunt cumque asperiores rerum natus doloribus labore sit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Debitis voluptatum ducimus quidem obcaecati veniam assumenda, excepturi exercitationem ut ad atque alias consequuntur dolor, saepe quis maxime repudiandae non, libero doloremque.";
+    }
+
+    function promedio($n1, $n2){
+        return ($n1 + $n2) /2;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -12,14 +22,15 @@
     <body>
         <section>
             <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Neque libero vero corrupti omnis? Beatae magnam maiores veritatis doloremque harum! Quas repudiandae, est incidunt cumque asperiores rerum natus doloribus labore sit.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Debitis voluptatum ducimus quidem obcaecati veniam assumenda, excepturi exercitationem ut ad atque alias consequuntur dolor, saepe quis maxime repudiandae non, libero doloremque.
+                <?php echo loremBy();?>
+                <?php echo loremBy();?>
+                <?php echo loremBy();?>
+                <br>
+                <?php echo promedio(10, 10)?>
             </p>
         </section>
         <footer>
-            <h1>Todos los derechos reservados - 2024</h1>
+            <h1>Todos los derechos reservados - <?php echo date("Y")?></h1>
         </footer>
     </body>
 </html>
